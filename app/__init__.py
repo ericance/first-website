e9805486ce40956e81319a6d2cb3d61528105362
ericance <ericstalcup@gmail.com>
2023-04-30 23:19:23 -0500

    Navigation buttons and store

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,13 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/store')
+def store():
+	return render_template('store.html')
+
+@app.route('/cart')
+def cart():
+	return render_template('cart.html')
+
+
+
